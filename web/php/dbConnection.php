d69bf30c3b7e418e3540424d51eeb38d897826fd
Nicola Bortignon <info@nicolabortignon.com>
2016-01-09 21:07:33 -0800

    adding dbConnection php class.

---
+++ web/php/dbConnection.php
@@ -0,0 +1,19 @@
+<?php
+
+$ip = 'promo.tracklist.me';
+$user = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+$database = getenv('MYSQL_DB');
+$mysqli = new mysqli($ip, $user, $password, $database);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+echo $mysqli->host_info . "\n";
+
+$mysqli = new mysqli("127.0.0.1", "user", "password", "database", 3306);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+echo $mysqli->host_info . "\n";
+?>
