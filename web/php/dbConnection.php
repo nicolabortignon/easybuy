e1804aa0031a523021389aea2f172c1788674afc
Nicola Bortignon <info@nicolabortignon.com>
2016-02-09 13:07:03 -0800

    back to prod

--- web/php/dbConnection.php
+++ web/php/dbConnection.php
@@ -1,9 +1,13 @@
 <?php
 
-$ip = '127.0.0.1:8889';
-$user = 'root';
-$password = 'root';
-$database = 'nucleo5_easybuy';
+if(getenv('MYSQL_USER') === false){
+	include_once(dirname(__FILE__).'/../../settings');
+}
+$ip = '74.124.204.215';
+$user = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+$database = getenv('MYSQL_DB');
+
 
 $mysqli = new mysqli($ip, $user, $password, $database);
 if ($mysqli->connect_errno) {
